32e2d7cdf4d2d005298b1c1ad9bba192bbe33aaf
Fiach Reid <fiach.reid@gmail.com>
2018-03-27 13:21:46 +0100

    OK, we're in san francisco

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -35,7 +35,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         locationManager.requestWhenInUseAuthorization()
-        
+        locationManager.startUpdatingLocation()
         
     }
     
@@ -78,11 +78,22 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     
     //Write the didUpdateLocations method here:
-    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            
+            print("Lat = \(location.coordinate.latitude)")
+            print("Lon = \(location.coordinate.longitude)")
+        }
+    }
     
     
     //Write the didFailWithError method here:
-    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location unavailable"
+    }
     
     
 
