10a38074c84173f5aff6f7028a04b1e6b4e82909
Fiach Reid <fiach.reid@gmail.com>
2018-03-27 12:48:44 +0100

    Initial Commit

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -7,13 +7,14 @@
 //
 
 import UIKit
+import CoreLocation
 
 
 class WeatherViewController: UIViewController {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "6dc134ca74b9cfb3dbab50ebc7a31a79"
     
 
     //TODO: Declare instance variables here
